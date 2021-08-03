8a6e6b271d46a4128c8ad146ec51b292450b644b
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-08-02 23:23:36 -0300

    added input login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, StatusBar, Image, SafeAreaView } from 'react-native';
+import { Item, Input, Text, Button } from 'native-base';
+import logoApp from '../assets/chatLogo.png';
 
 export default function Login() {
   return (
-    <View>
-      <Text>Estamos en el Login</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <View>
+        <Image source={logoApp} resizeMode="contain" style={styles.logo} />
+      </View>
+      <View>
+        <Input
+          placeholder="Nombre de usuario"
+          style={{ color: '#FFF' }}
+          placeholderTextColor="grey"
+        />
+      </View>
+      <Button style={styles.btnLogin}>
+        <Text style={{ color: '#FFF' }}>Entrar</Text>
+      </Button>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 50,
+    marginVertical: 100,
+  },
+  logo: {
+    width: '100%',
+    height: 200,
+    marginBottom: 30,
+  },
+  btnLogin: {
+    marginTop: 40,
+    justifyContent: 'center',
+    backgroundColor: '#0098d3',
+  },
+});
